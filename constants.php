f8b234cd201a7d5021d8e4f9f4d990f8864c8a21
Pei Qi Tea <moneysdew@gmail.com>
2021-06-22 21:21:13 +0800

    feat: Run insert links query upon post save

--- constants.php
+++ constants.php
@@ -5,6 +5,8 @@ if (!defined('ABSPATH')) exit;
 class BrainJournal_Constants {
     public $table_prefix;
     public $charset_collate;
+
+    public $links_table_name;
     
     public const REST_NAMESPACE = 'brainjournal/v1';
     
@@ -14,5 +16,6 @@ class BrainJournal_Constants {
         // Obtain the table prefix and charset collate for use in other parts of the plugin
         $this->table_prefix = $wpdb->prefix;
         $this->charset_collate = $wpdb->get_charset_collate();
+        $this->links_table_name = "{$this->table_prefix}brainjournal_links";
     }
 }
--- src/brainjournal.links.php
+++ src/brainjournal.links.php
@@ -9,31 +9,38 @@ if (!defined('ABSPATH')) exit;
 
 class BrainJournal_Links
 {
+    private $table_name;
     private $create_links_table_query;
     private $drop_links_table_query;
 
     private $select_posts_with_links_query;
     private $mass_insert_query_start;
+    private $mass_delete_query_for_post;
+    private $parse_link_regex = '/<a href=.*? data-type="post" data-id="([0-9]+?)">/m';
 
     public function __construct(BrainJournal_Constants $constants)
     {
+        // Initialize the queries
+        $this->table_name = $constants->links_table_name;
         $this->create_links_table_query = "
-            CREATE TABLE IF NOT EXISTS {$constants->table_prefix}brainjournal_links (
+            CREATE TABLE IF NOT EXISTS {$this->table_name} (
                 source INT(11) NOT NULL,
                 target INT(11) NOT NULL,
                 PRIMARY KEY (source, target)
             ) {$constants->charset_collate};
         ";
 
-        $this->drop_links_table_query = "DROP TABLE IF EXISTS {$constants->table_prefix}brainjournal_links;";
+        $this->drop_links_table_query = "DROP TABLE IF EXISTS {$this->table_name};";
         $this->select_posts_with_links_query = "
             SELECT ID, post_content FROM {$constants->table_prefix}posts 
             WHERE post_type = 'post' AND 
             post_content REGEXP '<a href=.*? data-type=\"post\" data-id=\"[0-9]+?\">'
         ";
-        $this->mass_insert_query_start = "INSERT IGNORE INTO {$constants->table_prefix}brainjournal_links(source, target) VALUES ";
+        $this->mass_insert_query_start = "INSERT IGNORE INTO {$this->table_name}(source, target) VALUES ";
+        $this->mass_delete_query_for_post = "DELETE FROM {$this->table_name} WHERE source = %d OR target = %d";
 
-        // add_action('save_post', [$this, "update_links"]);
+        // Call the update_links function whenever the post is saved
+        add_action('save_post', [$this, "update_links"], 10, 2);
     }
 
     // Call this whenever the plugin is activated
@@ -55,26 +62,53 @@ class BrainJournal_Links
 
         foreach ($posts as $p) {
             $source = $p["ID"];
-            $targets = [];
-            preg_match_all('/<a href=.*? data-type="post" data-id="([0-9]+?)">/m', $p["post_content"], $targets);
-
-            foreach (array_slice($targets, 1, count($targets) - 1) as $target_arr) {
-                foreach ($target_arr as $target) {
-                    if (trim($target) != "") {
-                        $values[] = $wpdb->prepare("(%d, %d)", $source, $target);
-                    }
-                }
-            }
+            $values = array_merge($values, $this->prepare_insert_values($source, $p["post_content"]));
         }
 
         $final_query = $this->mass_insert_query_start . implode(",", $values);
         $wpdb->query($final_query);
     }
 
-    // call this whenever a post is saved
-    function update_links($post_content, $post_id)
+    function prepare_insert_values($source, $post_content)
     {
-        // SELECT id, post_content FROM `pkm_posts` WHERE post_type = 'post' and post_content REGEXP '<a href=".*? data-type="post" data-id="\d+?">'
+        global $wpdb;
+
+        $targets = [];
+        $values = [];
+
+        preg_match_all($this->parse_link_regex, $post_content, $targets);
+
+        foreach (array_slice($targets, 1, count($targets) - 1) as $target_arr) {
+            foreach ($target_arr as $target) {
+                if (trim($target) != "") {
+                    $values[] = $wpdb->prepare("(%d, %d)", $source, $target);
+                }
+            }
+        }
+
+        return $values;
+    }
+
+    // call this whenever a post is saved
+    function update_links($post_ID, $post)
+    {
+        global $wpdb;
+
+        // Note: If you are updating a post, the post_type would be "revision"
+        // and that has its own ID! But we only want to get the original post's ID
+        // Hence, account for this difference in ID.
+        $original_post_ID = ($post->post_type == "revision") ? $post->post_parent : $post_ID;
+
+        $values = $this->prepare_insert_values($original_post_ID, $post->post_content);
+        
+        if (count($values) > 0) {
+            // Remove existing values
+            $wpdb->query($wpdb->prepare($this->mass_delete_query_for_post, $original_post_ID, $original_post_ID));
+
+            // Insert new values
+            $final_query = $this->mass_insert_query_start . implode(",", $values);
+            $wpdb->query($final_query);
+        }
     }
 
 
